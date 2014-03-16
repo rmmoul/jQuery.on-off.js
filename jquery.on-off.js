1c6de5186b0f0c8872f3c177d524b2d6f12766a9
Raymond Moul <raymond@raymondmoul.com>
2014-03-16 14:09:31 -0400

    Create jquery.on-off.js
    Added swap and preloader code

---
+++ jquery.on-off.js
@@ -0,0 +1,18 @@
+Jquery(document).ready(function($) {
+	//image hover magic
+	$('img.rollover').hover(
+		function() { this.src = this.src.replace('_off', '_on');},
+		function() { this.src = this.src.replace('_on', '_off');}
+	);
+
+	//auto-preload the swap states
+	var imgs = new Array();
+	$('img.rollover').each(function(idx){
+		var i = new Image();
+		i.src = $(this).attr('src').replace('_off', '_on');
+		alert(i.src);
+		imgs.push(i);
+	});
+});
+
+
